9e4ec7cf3e6d3cdabd300648c09422ed5714076c
Vietor Liu <vietor.liu@gmail.com>
2020-04-30 15:34:55 +0800

    Clean some code

--- src/main/scala/syncd/mongodb/Transmission.scala
+++ src/main/scala/syncd/mongodb/Transmission.scala
@@ -12,127 +12,143 @@ import com.mongodb.client.MongoCollection
 
 object MgTransmission {
 
+  private object DBOUtil {
+    def child(o: DBObject, k: String): DBObject = {
+      o.get(k).asInstanceOf[DBObject]
+    }
+
+    def pick(o: DBObject, include_fields: ArrayList[String]): DBObject = {
+      val resp = {
+        if(include_fields == null || include_fields.size() < 1)
+          o
+        else {
+          new BasicDBObject() {
+            for(k <- o.keySet().asScala) {
+              if(include_fields contains k)
+                put(k, o.get(k))
+            }
+          }
+        }
+      }
+      if(resp.containsField("_id"))
+        resp.removeField("_id")
+      resp
+    }
+
+    def includes(o: DBObject, include_fields: ArrayList[String]): Boolean = {
+      if(include_fields == null || include_fields.size() < 1)
+        true
+      else {
+        o.keySet().asScala.exists(k => {
+          if(k == "_id")
+            false
+          else {
+            val pos = k.indexOf(".")
+            if(pos == -1)
+              include_fields contains k
+            else
+              include_fields contains k.substring(0, pos)
+          }
+        })
+      }
+    }
+
+    def pickForUpdate(o: DBObject, include_fields: ArrayList[String]): (BasicDBObject, BasicDBObject) = {
+      var parted = false
+      val exist_fields = new ArrayList[String]
+      val resp = {
+        new BasicDBObject() {
+          if(include_fields == null || include_fields.size() < 1) {
+            for(k <- o.keySet().asScala) {
+              if(k != "_id") {
+                val pos = k.indexOf(".")
+                if(pos == -1) {
+                  put(k, o.get(k))
+                  exist_fields.add(k)
+                }
+                else {
+                  parted = true
+                  exist_fields.add(k.substring(0, pos))
+                }
+              }
+            }
+          }
+          else {
+            for(k <- o.keySet().asScala) {
+              if(k != "_id") {
+                val pos = k.indexOf(".")
+                if(pos == -1) {
+                  if(include_fields contains k) {
+                    put(k, o.get(k))
+                    exist_fields.add(k)
+                  }
+                } else {
+                  val sk = k.substring(0, pos)
+                  if(include_fields contains sk) {
+                    parted = true
+                    exist_fields.add(sk)
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      ({
+        if(!parted)
+          null
+        else
+          new BasicDBObject() {
+            exist_fields.forEach(k =>
+              append(k, 1)
+            )
+          }
+      }, resp)
+    }
+  }
+
   case class ImportContext(
+    config: MgConfig,
     client: MongoClient,
-    config: MgConfig
+    queryFields: BasicDBObject,
+    querySort: BasicDBObject
   )
 
-  private def filterDBObject(config: MgConfig, o: DBObject): DBObject = {
-    val resp = {
-      if(config.include_fields == null || config.include_fields.size() < 1)
-        o
-      else {
-        val out = new BasicDBObject() {
-          for(k <- o.keySet().asScala) {
-            if(config.include_fields contains k)
-              put(k, o.get(k))
-          }
-        }
-        out
-      }
-    }
-    if(resp.containsField("_id"))
-      resp.removeField("_id")
-    resp
-  }
-
-  private def hasIncludeFields(config: MgConfig, o: DBObject): Boolean = {
-    if(config.include_fields == null || config.include_fields.size() < 1)
-      true
-    else {
-      o.keySet().asScala.exists(k => {
-        if(k == "_id")
-          false
-        else {
-          val pos = k.indexOf(".")
-          if(pos == -1)
-            config.include_fields contains k
-          else
-            config.include_fields contains k.substring(0, pos)
-        }
-      })
-    }
-  }
-
-  private def filterUpdateDBObject(config: MgConfig, o: DBObject): (BasicDBObject, BasicDBObject) = {
-    var parted = false
-    val fields = new ArrayList[String]
-    val resp = {
-      new BasicDBObject() {
-        if(config.include_fields == null || config.include_fields.size() < 1) {
-          for(k <- o.keySet().asScala) {
-            if(k != "_id") {
-              val pos = k.indexOf(".")
-              if(pos == -1) {
-                fields.add(k)
-                put(k, o.get(k))
-              }
-              else {
-                val sk = k.substring(0, pos)
-                parted = true
-                fields.add(sk)
-              }
-            }
-          }
-        }
-        else {
-          for(k <- o.keySet().asScala) {
-            if(k != "_id") {
-              val pos = k.indexOf(".")
-              if(pos == -1) {
-                if(config.include_fields contains k) {
-                  fields.add(k)
-                  put(k, o.get(k))
-                }
-              } else {
-                val sk = k.substring(0, pos)
-                if(config.include_fields contains sk) {
-                  parted = true
-                  fields.add(sk)
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    ({
-      if(!parted)
-        null
-      else
-        new BasicDBObject() {
-          fields.forEach(k =>
-            append(k, 1)
-          )
-        }
-    }, resp)
-  }
+  case class OplogContext(
+    config: MgConfig,
+    client: MongoClient,
+    fetchFields: BasicDBObject,
+    oplogName: String,
+    masterCollection: MongoCollection[BasicDBObject]
+  )
 
   def createImportContext(cluster: MgClusterNode, config: MgConfig): ImportContext = {
-    ImportContext(cluster.getClient(), config)
+    ImportContext(
+      config,
+      cluster.getClient(),
+      {
+        if(config.include_fields == null || config.include_fields.size() < 1)
+          new BasicDBObject()
+        else {
+          new BasicDBObject() {
+            put("_id", 1: Integer)
+            config.include_fields.forEach(key =>
+              put(key, 1: Integer)
+            )
+          }
+        }
+      },
+      new BasicDBObject("_id", 1)
+    )
   }
 
   def importCollection(context: ImportContext, retry:()=> Int, iterate: (MgRecord) => Unit): Unit = {
-    val client = context.client
     val config = context.config
-
-    val theProjection = {
-      if(config.include_fields == null || config.include_fields.size() < 1)
-        new BasicDBObject()
-      else {
-        new BasicDBObject() {
-          put("_id", 1: Integer)
-          config.include_fields.forEach(key =>
-            put(key, 1: Integer)
-          )
-        }
-      }
-    }
-    val theSort = new BasicDBObject("_id", 1)
 
     var lastId: Object = null
     var inProgress: Boolean = true
-    val collection = client.getDatabase(config.db).getCollection(config.collection, classOf[BasicDBObject])
+    val collection = context.client.getDatabase(config.db).getCollection(config.collection, classOf[BasicDBObject])
+
     while(inProgress) {
       try {
         Using.resource(collection.find({
@@ -140,12 +156,12 @@ object MgTransmission {
             new BasicDBObject("_id", new BasicDBObject("$gt", lastId))
           else
             new BasicDBObject()
-        }).projection(theProjection).sort(theSort).iterator()) {
+        }).projection(context.queryFields).sort(context.querySort).iterator()) {
           cursor => {
             while (cursor.hasNext()) {
               val row =  cursor.next();
               lastId = row.get("_id")
-              iterate(MgRecord(row.get("_id"), filterDBObject(config, row)))
+              iterate(MgRecord(row.get("_id"), DBOUtil.pick(row, config.include_fields)))
             }
             inProgress = false;
           }
@@ -165,19 +181,10 @@ object MgTransmission {
     }
   }
 
-  case class OplogContext(
-    config: MgConfig,
-    client: MongoClient,
-    collection: MongoCollection[BasicDBObject],
-    projection: BasicDBObject,
-    oplogName: String
-  )
-
   def createOplogContext(cluster: MgClusterNode, shard: MgShardNode, config: MgConfig): OplogContext = {
     OplogContext(
       config,
       MgClientUtils.createClient(shard.replicas, config.cluster.auth),
-      cluster.getClient().getDatabase(config.db).getCollection(config.collection, classOf[BasicDBObject]),
       {
         if(config.include_fields == null || config.include_fields.size() < 1)
           null
@@ -189,51 +196,34 @@ object MgTransmission {
           }
         }
       },
-      shard.oplogName
+      shard.oplogName,
+      cluster.getClient().getDatabase(config.db).getCollection(config.collection, classOf[BasicDBObject]),
     )
   }
 
   def syncCollectionOplog(context: OplogContext, timestamp: MgTimestamp, iterate: (MgOpRecord) => Unit): Unit = {
-    val client = context.client
     val config = context.config
-    val collection = context.collection
-    val projection = context.projection
-
-    def getDBObject(row: DBObject, k: String): DBObject = {
-      row.get(k).asInstanceOf[DBObject]
-    }
-
-    def mergeDBObject(a: DBObject, b: DBObject): DBObject = {
-      val out = new BasicDBObject() {
-        for(k <- a.keySet().asScala) {
-          put(k, a.get(k))
-        }
-        for(k <- b.keySet().asScala) {
-          put(k, b.get(k))
-        }
-      }
-      out
-    }
 
     def isInternalMoving(row: DBObject): Boolean = {
       val KEY = "fromMigrate"
       row.containsField(KEY) && row.get(KEY).asInstanceOf[Boolean]
     }
 
-    def fetchOriginDBObject(timestamp: MgTimestamp, op: Int, _id: Object, fields: BasicDBObject): MgOpRecord = {
-      Using.resource(collection.find(new BasicDBObject("_id", _id)).projection(fields).iterator()) {
+    def fetchFromMaster(timestamp: MgTimestamp, op: Int, _id: Object, fields: BasicDBObject): MgOpRecord = {
+      Using.resource(context.masterCollection.find(new BasicDBObject("_id", _id)).projection(fields).iterator()) {
         cursor => {
           if(!cursor.hasNext())
             MgOpRecord(timestamp, MgConstants.OP_IGNORE)
           else
-            MgOpRecord(timestamp, op, _id, filterDBObject(config, cursor.next()))
+            MgOpRecord(timestamp, op, _id, DBOUtil.pick(cursor.next(), config.include_fields))
         }
       }
     }
 
     val ns = config.db + "." + config.collection
-    val oplogrs = client.getDatabase(MgConstants.LOCAL_DATABASE).getCollection(context.oplogName, classOf[BasicDBObject])
-    Using.resource(oplogrs.find(new BasicDBObject("ts", new BasicDBObject("$gt", MgClientUtils.convertTimestamp(timestamp)))).cursorType(CursorType.TailableAwait).noCursorTimeout(true).oplogReplay(true).iterator()) {
+    val oplogCollection = context.client.getDatabase(MgConstants.LOCAL_DATABASE).getCollection(context.oplogName, classOf[BasicDBObject])
+
+    Using.resource(oplogCollection.find(new BasicDBObject("ts", new BasicDBObject("$gt", MgClientUtils.convertTimestamp(timestamp)))).cursorType(CursorType.TailableAwait).noCursorTimeout(true).oplogReplay(true).iterator()) {
       cursor => {
         while(cursor.hasNext()) {
           val row = cursor.next()
@@ -246,22 +236,22 @@ object MgTransmission {
             else
               row.get("op").toString match {
                 case "i" => {
-                  val doc = getDBObject(row, "o")
-                  MgOpRecord(current, MgConstants.OP_CREATE, doc.get("_id"), filterDBObject(config, doc))
+                  val doc = DBOUtil.child(row, "o")
+                  MgOpRecord(current, MgConstants.OP_CREATE, doc.get("_id"), DBOUtil.pick(doc, config.include_fields))
                 }
                 case "u" => {
-                  val _id = getDBObject(row, "o2").get("_id")
-                  val update = getDBObject(row, "o")
+                  val _id = DBOUtil.child(row, "o2").get("_id")
+                  val update = DBOUtil.child(row, "o")
                   if(!update.keySet().asScala.exists(x => x.substring(0, 1) == "$"))
-                    MgOpRecord(current, MgConstants.OP_RECREATE, _id, filterDBObject(config, update))
-                  else if(update.containsField("$unset") && hasIncludeFields(config, getDBObject(update, "$unset")))
-                    fetchOriginDBObject(current, MgConstants.OP_RECREATE, _id, projection)
+                    MgOpRecord(current, MgConstants.OP_RECREATE, _id, DBOUtil.pick(update, config.include_fields))
+                  else if(update.containsField("$unset") && DBOUtil.includes(DBOUtil.child(update, "$unset"), config.include_fields))
+                    fetchFromMaster(current, MgConstants.OP_RECREATE, _id, context.fetchFields)
                   else if(!update.containsField("$set"))
                     MgOpRecord(current, MgConstants.OP_IGNORE)
                   else{
-                    val (parted, doc) = filterUpdateDBObject(config, getDBObject(update, "$set"))
+                    val (parted, doc) = DBOUtil.pickForUpdate(DBOUtil.child(update, "$set"), config.include_fields)
                     if(parted != null)
-                      fetchOriginDBObject(current, MgConstants.OP_UPDATE, _id, parted)
+                      fetchFromMaster(current, MgConstants.OP_UPDATE, _id, parted)
                     else {
                       if(doc.keySet().size() < 1)
                         MgOpRecord(current, MgConstants.OP_IGNORE)
@@ -271,7 +261,7 @@ object MgTransmission {
                   }
                 }
                 case "d" => {
-                  val _id = getDBObject(row, "o").get("_id")
+                  val _id = DBOUtil.child(row, "o").get("_id")
                   MgOpRecord(current, MgConstants.OP_DELETE, _id)
                 }
                 case _ => {
